6fa7214e2bc04fe4bcfcc413583ca15ef6ebd98a
CamiloMedel <129447046+CamiloMedel@users.noreply.github.com>
2025-03-05 22:03:03 -0600

    form validation

--- src/Components/BookingForm.js
+++ src/Components/BookingForm.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
 
+import '../Css/BookingForm.css';
+
 function BookingForm(props) {
     const [fieldData, setFieldData] = useState({
         date: "",
@@ -8,10 +10,39 @@ function BookingForm(props) {
         occasion: ""
     })
 
+    const [fieldErrors, setFieldErrors] = useState({
+        "date-error": "",
+        "res-time-error": "",
+        "guests-error": "",
+        "occasion-error": ""
+    })
+
+    const handleError = (name, value) => {setFieldErrors(prevErrors => ({...prevErrors, [name] : value}))}
+
+    const isThereAnyErrors = () => {
+        return !(fieldErrors['date-error'] === "" && fieldErrors['res-time-error'] === "" && fieldErrors['guest-error'] === "" && fieldErrors['occasion-error'] === "")
+    }
+
     const handleChange = e => {
         const name = e.target.name;
         const value = e.target.value;
-        if (name === "date") {props.dispatchAvailableTimes(value)}
+        if (name === "date") {
+            const invalidDateThresh = new Date()
+            invalidDateThresh.setDate(invalidDateThresh.getDate()-2)
+            if(new Date(value) < invalidDateThresh){
+                handleError("date-error", "Can not book for a date that has passed");
+            }
+            else{
+                handleError("date-error", "");
+                props.dispatchAvailableTimes(value);
+            }
+        } else if (name == "res-time"){
+            if(value === "--select--"){
+                handleError("res-time-error", "Please select an option");
+            } else {
+                handleError("res-time-error", "");
+            }
+        }
 
         setFieldData(prevFieldData => ({...prevFieldData, [name]: value}))
     }
@@ -24,19 +55,27 @@ function BookingForm(props) {
     return (
         <form style={{"display": "grid", "max-width": "200", "gap": "20"}} onSubmit={handleSubmit}>
             <label for="res-date">Choose date</label>
-            <input type="date" id="res-date" name="date" value={fieldData['date']} onChange={handleChange}/>
+            <input type="date" id="res-date" name="date" value={fieldData['date']} onChange={handleChange} required className={fieldErrors['date-error'] === "" ? "no-error" : "error"}/>
+            <p style={fieldErrors['date-error'] === "" ? {} : {"display": "block"}}>{fieldErrors['date-error']}</p>
             <label for="res-time">Choose time</label>
-            <select id="res-time" name="res-time" onChange={handleChange}>
-                {props.availableTimes.map((time) => <option>{time}</option>)}
+            <select id="res-time" name="res-time" value={fieldData['res-time']} onChange={handleChange}>
+                <option value="--select--" key="select"></option>
+                {props.availableTimes.map((time) => <option value={time} key={time}>{time}</option>)}
             </select>
+            <p style={fieldErrors['res-time-error'] === "" ? {} : {"display": "block"}}>{fieldErrors['res-time-error']}</p>
             <label for="guests">Number of guests</label>
-            <input type="number" placeholder="1" min="1" max="10" id="guests" name="guests" onChange={handleChange}/>
+            <input type="number" placeholder="1" min="1" max="10" id="guests" name="guests" value={fieldData['guests']} onChange={handleChange}/>
             <label for="occasion">Occasion</label>
-            <select id="occasion" name="occasion" onChange={handleChange}>
+            <select id="occasion" name="occasion" value={fieldData['occasion']} onChange={handleChange}>
                 <option>Birthday</option>
                 <option>Anniversary</option>
             </select>
-            <input type="submit" value="Make Your reservation"/>
+            <input type="submit" value="Make Your reservation" role="submit" disabled={
+                !(fieldErrors['date-error'] === ""
+                && fieldErrors['res-time-error'] === ""
+                && fieldErrors['guests-error'] === ""
+                && fieldErrors['occasion-error'] === ""
+                )}/>
         </form>
     )
 }
--- src/Test/BookingForm.test.js
+++ src/Test/BookingForm.test.js
@@ -1,4 +1,4 @@
-import {render, screen} from "@testing-library/react"
+import {render, fireEvent, waitFor, screen} from "@testing-library/react"
 import BookingForm from "../Components/BookingForm";
 
 test("Renders static date label", () => {
@@ -6,3 +6,33 @@ test("Renders static date label", () => {
     const dateLabel = screen.getByText("Choose date");
     expect(dateLabel).toBeInTheDocument();
 });
+
+describe("Validate HTML5 form validation", () => {
+    test("Date input field is required", () => {
+        render(<BookingForm availableTimes={["1", "2"]} dispatchAvailableTimes={(times) => times} submitForm={() => true}/>)
+        const submitButton = screen.getByRole("submit")
+
+        const dateInput = screen.getByLabelText(/Choose date/);
+
+        fireEvent.click(submitButton);
+        expect(dateInput.validity.valid).toBe(false);
+    });
+    test("Number of guest input field is must be less greater than or equal to one and less than or equal to 10", ()=>{
+        render(<BookingForm availableTimes={["1", "2"]} dispatchAvailableTimes={(times) => times} submitForm={() => true}/>)
+        const submitButton = screen.getByRole("submit")
+
+        const numberOfGuestInput = screen.getByLabelText(/Number of guest/);
+
+        fireEvent.change(numberOfGuestInput, {target: {value: .5}});
+        fireEvent.click(submitButton);
+        expect(numberOfGuestInput.validity.valid).toBe(false);
+
+        fireEvent.change(numberOfGuestInput, {target: {value: 7}});
+        fireEvent.click(submitButton);
+        expect(numberOfGuestInput.validity.valid).toBe(true);
+
+        fireEvent.change(numberOfGuestInput, {target: {value: 11}});
+        fireEvent.click(submitButton);
+        expect(numberOfGuestInput.validity.valid).toBe(false);
+    });
+})
