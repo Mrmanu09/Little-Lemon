aa737286bafe981b675707c1cd66b5e38fec82fe
CamiloMedel <129447046+CamiloMedel@users.noreply.github.com>
2025-03-07 14:59:23 -0600

    improved header v2 and responsive implementations

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,28 @@
+import React, { useEffect, useRef } from "react";
+
 import '../Css/Header.css'
 import Nav from "./Nav";
 import logo from '../Assets/Logo 16@4x.png'
 
 function Header() {
+
+    const headerBox = useRef(null);
+    useEffect(() => {
+        let prevScrollPos = window.scrollY;
+        const handleScroll = () => {
+            if (window.scrollY > prevScrollPos) {
+                headerBox.current.style.transform = "translateY(-200px)";
+            } else {
+                headerBox.current.style.transform = "translateY(0px)";
+            }
+            prevScrollPos = window.scrollY;
+        }
+        window.addEventListener('scroll', handleScroll);
+        return () => { window.removeEventListener('scroll', handleScroll); }
+    }, []);
+
     return (
-        <header>
+        <header ref={headerBox}>
             <img alt="Little Lemon Logo" src={logo} width="193.04px" height="54px"/>
             <Nav/>
         </header>
