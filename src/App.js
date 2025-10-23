37150c29130c0294792c15772af4509da96c83c7
Manoj Anjana Kumar Kandregula <www.manojkandregula2005@gmail.com>
2025-10-24 00:10:38 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,37 @@ import './Css/App.css';
 import Header from './Components/Header';
 import Main from './Components/Main';
 import Footer from './Components/Footer';
+import { useEffect } from 'react';
+import Lenis from '@studio-freight/lenis';
 
 function App() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      smooth: true,
+      smoothTouch: true,
+      direction: "vertical",
+      gestureDirection: "vertical",
+      touchMultiplier: 2,
+    });
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
   return (
     <div id="app">
-      <Header/>
-      <Main/>
-      <Footer/>
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
